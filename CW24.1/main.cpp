a3719a680112d86bf09d4ab5d6c21df0f01389f9
Flexshu <egor.wm202@gmail.com>
2025-03-07 16:50:40 +0200

    Initial Commit

---
+++ CW24.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CW24.1
+//
+//  Created by Єгор on 07.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
